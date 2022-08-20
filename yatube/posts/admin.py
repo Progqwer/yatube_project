e043786ff8db77dc5ec9f1f6c613e9c7c273f9e5
Илья Цыганков <ilyatsygancov@gmail.com>
2022-08-20 21:33:56 +0300

    готовый проект

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,19 +1,18 @@
 from django.contrib import admin
-from .models import Post
-from .models import Group
-
+from .models import Post, Group
 
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
-    #это свойсво позволит сенять поля в любом посте
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    )
     list_editable = ('group',)
-    # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
-    # Это свойство сработает для всех колонок: где пусто — там будет эта строка 
-    empty_value_display = '-пусто-'  
+    empty_value_display = '-пусто-' 
 
-admin.site.register(Post, PostAdmin) 
+admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
 from turtle import title
-from django.shortcuts import render
 from .models import Post, Group
 from django.shortcuts import render, get_object_or_404
 
@@ -7,14 +6,13 @@ from django.shortcuts import render, get_object_or_404
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
-    title = 'YaTube'
+    title = 'Последние обновления на сайте'
     context = {
         'title': title,
         'text': 'Это главная страница YaTube',
         'posts': posts,
     }
     return render(request, template, context) 
-
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
